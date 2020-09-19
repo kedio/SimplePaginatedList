9bc82b3e2c5dccd9c3e2bb0f7e8bb319481012e8
Kevin Dion <dion.kevin.1@gmail.com>
2020-09-19 13:42:45 -0300

    Some clean up and name refactoring of the initial project

--- SimplePaginatedList/Views/FactoryListViewController.swift
+++ SimplePaginatedList/Views/FactoryListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FactoryListViewController.swift
 //  SimplePaginatedList
 //
 //  Created by Kevin Dion on 2020-09-19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FactoryListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
