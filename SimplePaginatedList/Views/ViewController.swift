17efc06e1edf3b7cf64264e5afaef55ce2c28ab6
Kevin Dion <dion.kevin.1@gmail.com>
2020-09-19 13:20:06 -0300

    Initial project structure commit

---
+++ SimplePaginatedList/Views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimplePaginatedList
+//
+//  Created by Kevin Dion on 2020-09-19.
+//  Copyright © 2020 Kevin Dion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
